736d199f9f45d96eb71df89c746c584bd6dfcb74
Abdul-fatai <staticdev20046@gmail.com>
2019-08-06 12:11:17 +0100

    first commit

---
+++ config/eloquent-oauth.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\User;
+
+return [
+    'model' => User::class,
+    'table' => 'oauth_identities',
+    'providers' => [
+        'facebook' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/facebook/redirect',
+            'scope' => [],
+        ],
+        'google' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/google/redirect',
+            'scope' => [],
+        ],
+        'github' => [
+            'client_id' => 'a5952c9aa9517c3b2683',
+            'client_secret' => 'c29f3204eefe1101c7585b94b0c80599396f1e74',
+            'redirect_uri' => 'http://forum.ly/github/redirect',
+            'scope' => [],
+        ],
+        'linkedin' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/linkedin/redirect',
+            'scope' => [],
+        ],
+        'instagram' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/instagram/redirect',
+            'scope' => [],
+        ],
+        'soundcloud' => [
+            'client_id' => '12345678',
+            'client_secret' => 'y0ur53cr374ppk3y',
+            'redirect_uri' => 'https://example.com/your/soundcloud/redirect',
+            'scope' => [],
+        ],
+    ],
+];
---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Social Routes
+
+Route::get('{provider}/auth', [
+    'uses' => 'SocialsController@auth',
+    'as' => 'social.auth'
+]);
+
+Route::get('/{provider}/redirect', [
+    'uses' => 'SocialsController@auth_callback',
+    'as' => 'social.callback'
+]);
