f6bfcee25632abea103aed38a1aca985eea4affc
Abdul-fatai <staticdev20046@gmail.com>
2019-08-17 12:11:48 +0100

    new update

--- resources/views/discuss.blade.php
+++ resources/views/discuss.blade.php
@@ -1,19 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="card">
+        <div class="card-header">Create a new Discussion</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+        <div class="card-body">
+           <form action="{{ route('discussions.store') }} " method="POST">
+                {{ csrf_field() }}
 
-                    You are logged in!
+                <div class="form-group">
+                    <label for="channel">Pick a channel</label>
+                    <select name="channel_id" id="channel_id" class="form-control">
+                        @foreach($channels as $channel)
+                            <option value="{{ $channel->id }}">{{ $channel->title }}</option>
+                        @endforeach
+                    </select>
                 </div>
-            </div>
+
+                <div class="form-group">
+                    <label for="content">Ask a question</label>
+                    <textarea name="content" id="content" cols="30" rows="10" class="form-control"></textarea>
+                </div>
+                
+                <div class="form-group">
+                    <button class="btn btn-primary pull-center">Create discussion</button>
+                </div>
+           </form>
         </div>
+    </div>
 @endsection
